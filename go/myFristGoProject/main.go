1ca116348213503446718ede942ac9c656c467f0
Natanael Roberto Rodrigues <natanaelrodrigues@gmail.com>
2024-12-02 16:29:34 -0300

    definição de variáveis

--- go/myFristGoProject/main.go
+++ go/myFristGoProject/main.go
@@ -4,4 +4,15 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, world!")
+
+	// declaração de variáveis
+	var nome, sobrenome string
+	novoNome := "Aluis"
+	var idade = 30
+
+	// usando as variáveis
+	nome = "Jose"
+	sobrenome = "Rodrigues"
+
+	fmt.Println(nome, sobrenome, novoNome, idade)
 }
