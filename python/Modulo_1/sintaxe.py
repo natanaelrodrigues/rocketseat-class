621a788013b006f30a2e8386dfc5451ca00f356a
Natanael Roberto Rodrigues <natanaelrodrigues@gmail.com>
2025-01-17 16:50:22 -0300

    sitaxe python

---
+++ python/Modulo_1/sintaxe.py
@@ -0,0 +1,26 @@
+# Comentário em linha
+
+
+"""
+Comentário em bloco
+Linha 1
+Linha 2
+Lonha 3
+"""
+
+# quebra de linha
+nome = "Natanael " + \
+"Rodrigues"
+
+print(nome)
+
+# Blocos de código
+
+idade  = 25
+
+if idade < 18:
+	print('Menor de idade')
+else:
+  print('Maior de idade')
+
+print("Fim de Programa!")
