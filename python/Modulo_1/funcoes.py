769b5744fe655631789926704390f11ff7463c89
Natanael R Rodrigues <natanaelrodrigues@gmail.com>
2025-02-10 21:36:35 -0300

    funcoes

---
+++ python/Modulo_1/funcoes.py
@@ -0,0 +1,16 @@
+def saudacao(nome):
+  print(f"Olá, {nome}!")
+
+print("Chamando a função saudação")
+saudacao("Natanael")
+saudacao("Joao")
+saudacao("Helena")
+
+# função com retorno
+def quadrado(num):
+  return num**2
+
+print("\n Chamando função quadrado")
+resultado = quadrado(5)
+print("Resultado:", resultado)
+
