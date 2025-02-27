e26d30e59756afa3356686c8b3ecd13579c69eb9
Natanael R Rodrigues <natanaelrodrigues@gmail.com>
2025-02-27 20:14:22 -0300

    Modelagem e configuração do Banco de dados.

--- python/Modulo_5_Notificacao_RealTime/app.py
+++ python/Modulo_5_Notificacao_RealTime/app.py
@@ -1,7 +1,13 @@
 from flask import Flask, jsonify
+from repository.database import db
+from db_models.payment import Payment
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'SECRET_KEY_WEBSOCKET'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
+db.init_app(app)
 
 @app.route('/payments/pix', methods=['POST'])
 def create_payment_pix():
