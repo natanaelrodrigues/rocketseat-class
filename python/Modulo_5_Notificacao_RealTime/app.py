9196019e9ef488da2126d3b4cacaf7f146895fde
Natanael Roberto Rodrigues <natanaelrodrigues@gmail.com>
2025-03-04 14:08:06 -0300

    implementando frontend de payment e rotas de incusão

--- python/Modulo_5_Notificacao_RealTime/app.py
+++ python/Modulo_5_Notificacao_RealTime/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_file, render_template
 from repository.database import db
 from db_models.payment import Payment
+from payments.pix import Pix
 from datetime import datetime, timedelta
 
 app = Flask(__name__)
@@ -21,12 +22,21 @@ def create_payment_pix():
 
   new_payment = Payment(value=data['value'], 
                         expiration_date=expiration_date)
-  
+  pix_obj = Pix();
+  data_payment_pix = pix_obj.create_payment();
+
+  new_payment.bank_payment_id = data_payment_pix["bank_payment_id"]
+  new_payment.qr_code = data_payment_pix["qr_code_path"]
+
   db.session.add(new_payment)
   db.session.commit()
 
   return jsonify({"message":"The payment has been created",
                   "payment": new_payment.to_dict()})
+
+@app.route('/payments/pix/qr_code/<file_name>',methods=['GET'])
+def get_image(file_name):
+  return send_file(f"static/img/{file_name}.png",mimetype='image/png')
 
 @app.route('/payments/pix/confirmation', methods=['POST'])
 def pix_confirmation():
@@ -34,7 +44,13 @@ def pix_confirmation():
 
 @app.route('/payments/pix/<int:payment_id>', methods=['GET'])
 def payment_pix_page(payment_id):
-  return "Pagamento Pix"
+  payment = Payment.query.get(payment_id)
+
+  return render_template('payment.html', 
+                         payment_id=payment.id, 
+                         value=payment.value, 
+                         host='http://127.0.0.1:5000', 
+                         qr_code=payment.qr_code )
 
 if __name__ == '__main__':
   app.run(debug=True)
---
+++ python/Modulo_5_Notificacao_RealTime/payments/pix.py
@@ -0,0 +1,24 @@
+import uuid;
+import qrcode;
+
+class Pix:
+  def __init__(self):
+    pass
+
+  def create_payment(self):
+    # criar o pagamento na instituição financeira
+    bank_payment_id =  str(uuid.uuid4());
+    
+    #qr Code - Copia e cola
+    hash_payment = f"hash_payment_{bank_payment_id}"
+
+    #qr Code
+    img = qrcode.make(hash_payment);
+
+    # Salva a imagem do código de barras como PNG
+    img.save(f"static/img/qr_code_payment_{bank_payment_id}.png")
+
+    return {
+      "bank_payment_id": bank_payment_id,
+      "qr_code_path": f"qr_code_payment_{bank_payment_id}"
+    }
