9069482909865ac6042799224c4e8d95a5d89987
Natanael R Rodrigues <natanaelrodrigues@gmail.com>
2025-03-05 20:18:17 -0300

    confirmation payd

--- python/Modulo_5_Notificacao_RealTime/app.py
+++ python/Modulo_5_Notificacao_RealTime/app.py
@@ -45,6 +45,25 @@ def get_image(file_name):
 
 @app.route('/payments/pix/confirmation', methods=['POST'])
 def pix_confirmation():
+  data = request.get_json();
+
+  # Validations
+  if "bank_payment_id" not in data and "value" not in data:
+    return jsonify({'message':'Invalid payment data'}), 400
+
+
+  payment = Payment.query.filter_by(bank_payment_id=data.get('bank_payment_id')).first()
+
+  if not payment or payment.paid:
+    return jsonify({'message':'Payment not found'}), 404
+  
+  if float(data.get('value')) != payment.value:
+    return jsonify({'message':'Invalid payment data'}), 400
+  
+  payment.paid = True
+  db.session.commit()
+
+
   return jsonify({"message":"The payment has been confirmed"})
 
 @app.route('/payments/pix/<int:payment_id>', methods=['GET'])
