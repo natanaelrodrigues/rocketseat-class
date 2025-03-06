7fd6ca09ea7f79bb57902bd88ea11c8690c9e742
Natanael Roberto Rodrigues <natanaelrodrigues@gmail.com>
2025-03-06 10:49:04 -0300

    page 404 e disconect socket

--- python/Modulo_5_Notificacao_RealTime/app.py
+++ python/Modulo_5_Notificacao_RealTime/app.py
@@ -70,6 +70,9 @@ def pix_confirmation():
 def payment_pix_page(payment_id):
   payment = Payment.query.get(payment_id)
 
+  if not payment:
+    return render_template('404.html')
+
   if payment.paid:
     return render_template('confirmed_payment.html',
                            payment_id=payment.id, 
@@ -86,6 +89,9 @@ def payment_pix_page(payment_id):
 def handle_connect():
   print('Client connected to the server...')
 
+@socketio.on('disconect')
+def handle_disconect():
+  print('Client has disconected to the server...')
 
 if __name__ == '__main__':
   socketio.run(app, debug=True)
